66669b104ff5c706d1e7827bf2be1c33aed023aa
ederpbj <ederpbj@yahoo.com.br>
2020-01-04 18:57:47 -0300

    6.6 EXIBINDO LISTA DE ALUNOS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const listaAlunos = document.querySelector('#listaAlunos');
+
 const GraphQl = {
     endpoint: 'https://eu1.prisma.sh/ederpbj-833032/cursos-online/dev',
 
@@ -27,3 +29,36 @@ const Aluno = {
         return GraphQl.exec(query);
     }
 }
+
+const Template ={
+    /*Pag 92: função que chame a função de busca de alunos, 
+    armazene os dados na lista e chame a função listarAluno() 
+    para exibirmos os dados na tela.
+    */
+    iniciar: function(){
+        Aluno.buscar()
+            .then(({data:{alunoes}}) => {
+                Aluno.lista = alunoes;
+                Template.listarAluno();
+            })
+    },
+    /**
+     * Sempre que quisermos que os dados do servidor sejam
+    exibidos, basta chamar Template.iniciar() . Para que nossa
+    aplicação já inicie com os dados sendo apresentados, adicione a
+    chamada desta função na última linha do arquivo scripts.js .
+     */
+    listarAluno: function () {
+        listaAlunos.innerHTML = html;
+    },
+
+    listarAluno: function(){
+        let html = ''
+        Aluno.lista.forEach((aluno) => {
+            html += `<li>Nome: ${aluno.nomeCompleto} - Idade: ${aluno.idade}`
+        })
+        listaAlunos.innerHTML = html;
+    }
+}
+
+Template.iniciar();
