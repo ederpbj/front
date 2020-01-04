bca826525cedd575b8b6edb0923f2d22101fc441
ederpbj <ederpbj@yahoo.com.br>
2020-01-04 18:37:24 -0300

    6.5 BUSCANDO DADOS

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const GraphQl = {
+    endpoint: 'https://eu1.prisma.sh/ederpbj-833032/cursos-online/dev',
+
+    exec: function(query, variaveis ){
+        return fetch(GraphQl.endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: query, variaveis: variaveis }),
+        }).then(resposta = resposta.json())
+    }
+}
+
+const Aluno = {
+    lista: [],
+    buscar: function(){
+        const query = `
+            query{
+                alunoes{
+                    id
+                    nomeCompleto
+                    idade
+                }
+            }
+        `;
+        return GraphQl.exec(query);
+    }
+}
+
